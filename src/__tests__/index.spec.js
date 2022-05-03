4577a5534391d7253aacfb419828ce3c67857c6c
Artur Dudnik <arthur.dudnik21@gmail.com>
2022-05-03 13:02:09 +0300

    ready

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -6,12 +6,58 @@ import {
   objectEntries,
 } from '../index';
 
-describe('changeStringCase()', () => {});
+describe('changeStringCase()', () => {
+  it('change word or no', function() {
+    expect(changeStringCase("Hello", "lowercase"))
+      .toEqual("hello");
+    expect(changeStringCase("Hello", "uppercase"))
+      .toEqual("HELLO");
+    expect(changeStringCase("HeLlo"))
+      .toEqual("HeLlo");
+  });
+});
 
-describe('sumOfArray()', () => {});
+describe('sumOfArray()', () => {
+  it('sum numbers', function() {
+    expect(sumOfArray([34, -34, 15, 6]))
+      .toEqual(21);
+    expect(sumOfArray([]))
+      .toEqual(0);
+    expect(sumOfArray([-34]))
+      .toEqual(-34);
+  });
+});
 
-describe('divisibleBy()', () => {});
+describe('divisibleBy()', () => {
+  it('filtering by division', function() {
+    expect(divisibleBy([21, 5, 25, 14, 15], 5))
+      .toStrictEqual([5, 25, 15]);
+    expect(divisibleBy([22, 5, 24, 14, 17], 2))
+      .toStrictEqual([22, 24, 14]);
+    expect(divisibleBy([21, 5, 25, 14, 15], 100))
+      .toStrictEqual([]);
+  });
+});
 
-describe('addDotsToString()', () => {});
+describe('addDotsToString()', () => {
+  it('add dots', function() {
+    expect(addDotsToString("Hello! My name is Artur", 20))
+      .toEqual("Hello! My name is...");
+    expect(addDotsToString("Hello! My name is Artur", 9))
+      .toEqual("Hello!...");
+    expect(addDotsToString("Hello! My name is Artur", 3))
+      .toEqual("...");
+    expect(addDotsToString("Hello! My name is Artur", 0))
+      .toEqual("Hello! My name is Ar...");
+  });
+});
 
-describe('objectEntries()', () => {});
+describe('objectEntries()', () => {
+  it('check structure of array', function() {
+    expect(objectEntries({name: "Artur",
+                                lastname: "Dudnik",
+                                age: 18
+    } ))
+      .toStrictEqual([["name", "Artur"], ["lastname", "Dudnik"], ["age", 18]]);
+  });
+});
